3102eb8424af98b88cf420fd3175a92c2ffa98ec
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-12-05 23:02:08 +0200

    make custom pipe validator to todoStatus

---
+++ src/pipes/TodoStatusValidation.pipe.ts
@@ -0,0 +1,29 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { TodoStatus } from 'src/Entity/todo.entity';
+
+export class TodoStatusValidationPipe implements PipeTransform {
+  readonly allowedStatus: TodoStatus[] = [
+    TodoStatus.OPEN,
+    TodoStatus.WIN,
+    TodoStatus.COMPLETED,
+  ];
+
+  transform(value, metadata: ArgumentMetadata): any {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} is an invalid staus.`);
+    }
+    return value;
+  }
+
+  private isStatusValid(status): boolean {
+    const index = this.allowedStatus.indexOf(status);
+
+    return index !== -1;
+  }
+}
