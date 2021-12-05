169e8ab90e2897736472aa1640aa4d8c9255d98d
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-12-05 23:00:21 +0200

    connect to database and make configration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const ormOptions: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '',
+  database: 'todo',
+  autoLoadEntities: true,
+  synchronize: true,
+};
 
 @Module({
-  imports: [],
+  imports: [TodoModule, TypeOrmModule.forRoot(ormOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
