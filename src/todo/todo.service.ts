c2c59b70619d6a75c840590296e410de1592f0e5
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-12-13 04:57:51 +0200

    add delete getAll update

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,5 +1,10 @@
+import { UserEntity } from './../Entity/user.entity';
 import { TodoEntity, TodoStatus } from 'src/Entity/todo.entity';
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DeleteResult, Repository } from 'typeorm';
 import { CreateTodoDTO } from 'src/DTO/create-todo.dto';
@@ -10,23 +15,39 @@ export class TodoService {
     @InjectRepository(TodoEntity) private repo: Repository<TodoEntity>,
   ) {}
   // get all todos
-  async getAllTodos() {
-    return await this.repo.find();
+  async getAllTodos(user: UserEntity) {
+    const query = await this.repo.createQueryBuilder('todo');
+
+    query.where(`todo.userId = :userId`, { userId: user.id });
+
+    try {
+      return await query.getMany();
+    } catch (err) {
+      throw new NotFoundException('No todo found');
+    }
   }
   // create new todo
-  async createTodo(createTodoDTO: CreateTodoDTO): Promise<TodoEntity> {
+  async createTodo(
+    createTodoDTO: CreateTodoDTO,
+    user: UserEntity,
+  ): Promise<TodoEntity> {
     const todo = new TodoEntity();
     todo.title = createTodoDTO.title;
     todo.description = createTodoDTO.description;
     todo.status = TodoStatus.OPEN;
+    todo.userId = user.id;
 
     this.repo.create(todo);
     return await this.repo.save(todo);
   }
   // update todo by id and status
-  async update(id: number, status: TodoStatus): Promise<TodoEntity> {
+  async update(
+    id: number,
+    status: TodoStatus,
+    user: UserEntity,
+  ): Promise<TodoEntity> {
     try {
-      await this.repo.update({ id }, { status });
+      await this.repo.update({ id, userId: user.id }, { status });
       return await this.repo.findOne({ id });
     } catch (err) {
       throw new InternalServerErrorException('Somthing went wrong');
@@ -34,11 +55,12 @@ export class TodoService {
   }
 
   // delete todo by id
-  async delete(id: number): Promise<DeleteResult> {
-    try {
-      return await this.repo.delete(id);
-    } catch (err) {
-      throw new InternalServerErrorException('Somthing went wrong');
+  async delete(id: number, user: UserEntity) {
+    const result = await this.repo.delete({ id, userId: user.id });
+    if (result.affected === 0) {
+      throw new NotFoundException('todo not deleted');
+    } else {
+      return { success: true };
     }
   }
 }
