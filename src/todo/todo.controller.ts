7a05c251e3cc7babbfd8adcf7666b705b6e00e9b
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-12-13 04:58:46 +0200

    implement auth and add CRUD to controller

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,3 +1,4 @@
+import { UserEntity } from './../Entity/user.entity';
 import { TodoStatus } from './../Entity/todo.entity';
 import {
   Body,
@@ -7,36 +8,44 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateTodoDTO } from 'src/DTO/create-todo.dto';
 import { TodoStatusValidationPipe } from 'src/pipes/TodoStatusValidation.pipe';
 import { TodoService } from './todo.service';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/auth/user.decorator';
 
 // http://localhost:3000/api/todos
 @Controller('todos')
+@UseGuards(AuthGuard())
 export class TodoController {
   constructor(private todoService: TodoService) {}
   @Get()
-  getAllTodos() {
-    return this.todoService.getAllTodos();
+  getAllTodos(@User() user: UserEntity) {
+    return this.todoService.getAllTodos(user);
   }
 
   @Post()
-  createNewTodo(@Body(ValidationPipe) data: CreateTodoDTO) {
-    return this.todoService.createTodo(data);
+  createNewTodo(
+    @Body(ValidationPipe) data: CreateTodoDTO,
+    @User() user: UserEntity,
+  ) {
+    return this.todoService.createTodo(data, user);
   }
 
   @Patch(':id')
   updateTodo(
     @Body('status', TodoStatusValidationPipe) status: TodoStatus,
     @Param('id') id: number,
+    @User() user: UserEntity,
   ) {
-    return this.todoService.update(id, status);
+    return this.todoService.update(id, status, user);
   }
 
   @Delete(':id')
-  deleteTodo(@Param('id') id: number) {
-    return this.todoService.delete(id);
+  deleteTodo(@Param('id') id: number, @User() user: UserEntity) {
+    return this.todoService.delete(id, user);
   }
 }
